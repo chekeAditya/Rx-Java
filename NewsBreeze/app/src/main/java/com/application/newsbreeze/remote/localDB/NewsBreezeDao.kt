6b5b0897c9b5c49bcdd6d815b49c39a31d991fa8
Aditya Cheke <adityacheke4@gmail.com>
2021-11-08 15:35:03 +0530

    Getting the repsonse from the api

--- NewsBreeze/app/src/main/java/com/application/newsbreeze/remote/localDB/NewsBreezeDao.kt
+++ NewsBreeze/app/src/main/java/com/application/newsbreeze/remote/localDB/NewsBreezeDao.kt
@@ -1,14 +1,14 @@
 package com.application.newsbreeze.remote.localDB
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import com.application.newsbreeze.remote.responses.Article
-
-@Dao
-interface NewsBreezeDao {
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun addDataFromAPI(article: List<Article>)
-
-}
+//import androidx.room.Dao
+//import androidx.room.Insert
+//import androidx.room.OnConflictStrategy
+//import com.application.newsbreeze.remote.responses.Article
+//
+//@Dao
+//interface NewsBreezeDao {
+//
+//    @Insert(onConflict = OnConflictStrategy.REPLACE)
+//    fun addDataFromAPI(article: List<Article>)
+//
+//}
--- NewsBreeze/app/src/main/java/com/application/newsbreeze/remote/localDB/NewsBreezeRoomDatabase.kt
+++ NewsBreeze/app/src/main/java/com/application/newsbreeze/remote/localDB/NewsBreezeRoomDatabase.kt
@@ -1,10 +1,11 @@
 package com.application.newsbreeze.remote.localDB
 
-import androidx.room.Database
-
-@Database(entities = [RoomModel::class],version = 1)
-abstract class NewsBreezeRoomDatabase {
-
-    abstract fun getResponseFromDao() : NewsBreezeDao
-
-}
+//import androidx.room.Database
+//import androidx.room.RoomDatabase
+//
+//@Database(entities = [RoomModel::class],version = 1)
+//abstract class NewsBreezeRoomDatabase : RoomDatabase() {
+//
+//    abstract fun getResponseFromDao() : NewsBreezeDao
+//
+//}
